ccadb16a3e26e12e140033b5dc341bd188eef7cb
fet1sov <august9222@gmail.com>
2023-11-17 23:22:29 +0500

    🌎 Web: Added the localization
    - Added the localization system based on GEO IP
    - Markdown for the index.ejs
    - Basic animations
    
    Posted this from Sharja, UAE 🇦🇪

--- pages.js
+++ pages.js
@@ -7,12 +7,30 @@ const router = express.Router(),
 const path = require('path');
 const fs = require('fs');
 
+const geoip = require('geoip-lite');
+
+
 // index
 router.get('/', function (request, response) {
+
+    var region = geoip.lookup(request.socket.remoteAddress);
+
+    var localeContent = "";
+    var localeJSON = "";
+
+    if (region)
+    {
+        localeContent = fs.readFileSync(`${__dirname}/views/locales/${region.country}`, 'utf8');
+        localeJSON = JSON.parse(localeContent);
+    } else {
+        localeContent = fs.readFileSync(`${__dirname}/views/locales/ru-RU.json`, 'utf8');
+        localeJSON = JSON.parse(localeContent);
+    }
 
     response.status(200);
     response.render('index', {
         title: 'Noise',
+        locale: localeJSON
     });
 });
 
